7474b711258a73e4937db148803b0c0e04dace20
Camille-lt <camillelebigot5@gmail.com>
2025-07-22 12:33:03 +0200

    add request sql of users

---
+++ bdd/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (firstname, lastname, email, password, status_id, user_biography)
+VALUES 
+  ('Eva', 'Tharrats', 'eva@travelers.com', 'mdpHashEva', 2, 'J’adore les volcans d’Islande.'),
+  ('Ahmed', 'Ayari', 'ahmed@travelers.com', 'mdpHashAhmed', 1, 'Toujours entre deux avions ✈️.'),
+  ('Camille', 'Lebigot', 'camille@travelers.com', 'mdpHashCamille', 1, 'En quête du plus beau coucher de soleil 🌅.');
