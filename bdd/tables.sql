1c3235289745658ada576de5aea82cadf9d28b23
EvaTP <evathpr@icloud.com>
2025-07-22 11:16:44 +0200

    fix: sql

--- bdd/tables.sql
+++ bdd/tables.sql
@@ -1,4 +1,8 @@
--- users
+CREATE TABLE status(
+"id" SERIAL PRIMARY KEY,
+"role" TEXT
+CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES users(id)
+)
 
 CREATE TABLE users (
     "id" SERIAL PRIMARY KEY,
@@ -6,7 +10,7 @@ CREATE TABLE users (
     "lastname" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-	"status_id" TEXT NOT NULL,
+	"status_id" INT NOT NULL,
 	"user_biography" TEXT,
     "created_at" timestamp NOT NULL DEFAULT (now()),
     "updated_at" timestamp NOT NULL DEFAULT (now())
@@ -20,19 +24,16 @@ CREATE TABLE posts (
 "user_id" INT,
 "created_at" timestamp NOT NULL DEFAULT (now()),
 "updated_at" timestamp NOT NULL DEFAULT (now())
-CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(user_id)
+CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
 )
 
 CREATE TABLE reactions(
  "id" SERIAL PRIMARY KEY,
  "comments" TEXT,
  "like" INT,
- "user_id" INT,
-CONSTRAINT fk_reaction FOREIGN KEY (user_id) REFERENCES users(user_id)
+ "user_id" INT NOT NULL,
+ "post_id" INT NOT NULL,
+CONSTRAINT fk_reaction FOREIGN KEY (user_id) REFERENCES users(id),
+CONSTRAINT fk_reaction_post FOREIGN KEY (post_id) REFERENCES posts (id)
 )
 
-CREATE TABLE status(
-"id" SERIAL PRIMARY KEY,
-"role" TEXT
-CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES users(status_id)
-)
